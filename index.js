0a1b64d958d393f57ff4d204befc713ba067eb9d
Taksh-p <takshpatel2353@gmail.com>
2025-05-04 13:04:11 +0530

    Remove the commented code & formate document & remove unused code

--- index.js
+++ index.js
@@ -1,22 +1,7 @@
 console.log("Welcome to attach the file.");
-
 const tagValue = document.getElementsByTagName(h1);
 
 if (tagValue)
     console.log("The Tag value is::: ", tagValue);
 else
     console.log("The Tag hase does not have any value");
-
-// if (tagValue)
-//     console.log("The Tag value is::: ", tagValue);
-// else
-//     console.log("The Tag hase does not have any value");
-
-if (tagValue)
-    console.log("The Tag value is::: ", tagValue);
-else
-    console.log("The Tag hase does not have any value");
-
-
-
-
